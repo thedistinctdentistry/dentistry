dfc1ef99c97c8602d8021c9198b86557c559c5f1
Distinct Dentistry Admin <admin@thedistinctdentistry.com>
2026-06-06 11:04:24 +0500

    Fix routing and hide about page

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
       <Route path="/services" element={<Services />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
