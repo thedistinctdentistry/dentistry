01fe0b18461cb50596c63923fe734c919adf9ce6
Distinct Dentistry Admin <admin@thedistinctdentistry.com>
2026-06-06 11:05:00 +0500

    Fix typescript unused import error in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
 import Contact from './pages/Contact';
 import Services from './pages/Services';
 
