31822835990ed0dd232efcdf1a83990e517ed746
Distinct Dentistry Admin <admin@thedistinctdentistry.com>
2026-06-07 12:07:46 +0500

    Re-enable About page and merge about-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
 import Contact from './pages/Contact';
 import Services from './pages/Services';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
       <Route path="/services" element={<Services />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
